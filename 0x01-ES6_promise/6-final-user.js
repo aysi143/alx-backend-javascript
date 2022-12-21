0ebeee182cfedd9d5c90ca4c6370b4fa39299248
ayssi143 <aysanewyonasj@gmail.com>
2022-12-21 22:15:07 +0300

    change minor catalogue | fix

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,14 +1,3 @@
-// Handle multiple promises
-	//import signUpUser from './4-user-promise';
-	//import uploadPhoto from './5-photo-reject';
-
-	//function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
-	//  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)]);
-	//}
-
-//export default handleProfileSignup;
-
-
 //Let's use the es6 in a cutest way
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
