e1fa99547633e5590ce567d3c6ba492b8f5e03f4
ayssi143 <aysanewyonasj@gmail.com>
2022-12-21 22:30:25 +0300

    code refactor | fix

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,23 +1,12 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  const s = signUpUser(firstName, lastName);
-  const u = uploadPhoto(fileName);
-  return Promise.allSettled([s, u]).then((vals) => {
-    const Arr = [];
-    vals.forEach((val) => {
-      if (val.status === 'fulfilled') {
-        Arr.push({ status: val.status, value: val.value });
-      } else {
-        Arr.push({
-          status: val.status,
-          value: `Error: ${val.reason.message}`,
-        });
-      }
-    });
-    return Arr;
-  });
+export default function handleProfileSignup(firstName, lastName, filename) {
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(filename),
+  ]).then((res) => res.map((item) => ({
+    status: item.status,
+    value: item.status === 'fulfilled' ? item.value : String(item.reason),
+  })));
 }
-
-
